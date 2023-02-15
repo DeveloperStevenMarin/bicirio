a5d859e77af772da00c3e6585230182086409bb9
DeveloperStevenMarin <developerstevenmarin@gmail.com>
2023-02-15 01:13:09 -0500

    Login create

---
+++ frontend/bicirio-web/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Routes, Route} from 'react-router';
+import Home from './pages/Home';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Login/>}/>
+      <Route path='/home' element={<Home/>}/>
+    </Routes>
+  );
+}
+
+export default App;
--- frontend/bicirio-web/src/pages/Login.js
+++ frontend/bicirio-web/src/pages/Login.js
@@ -1,30 +1,71 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import '../css/Login.css';
+import axios from 'axios';
+
+const baseUrl = "http://localhost:3001/user";
 
 class Login extends Component {
+    state = {
+        form: {
+            userID: '',
+            password: ''
+        }
+    }
+
+    handleChange = async e => {
+        await this.setState({
+            form: {
+                ...this.state.form,
+                [e.target.name]: e.target.value
+            }
+        });
+        console.log(this.state.form)
+    }
+
+    auth = async () => {
+        await axios.get(baseUrl, { params: { id: this.state.form.userID, password: this.state.form.password } }).then(response => {
+            console.log(response);
+        }).catch(error => {
+            console.log(error);
+        });
+    }
+
+
     render() {
         return (
-            <div className='LoginMainContainer'>
-                <div className='LoginSecondaryContainer'>
-                    <div className='form-group'>
-                        <label>Cédula: </label>
-                        <br />
-                        <input
-                            type="text"
-                            className='form-control'
-                        />
-                        <br />
-                        <label>Contraseña: </label>
-                        <br />
-                        <input
-                            type="password"
-                            className='form-control'
-                        />
+            <div className="MainContainer">
 
-                    </div>
+                <div className="form-group">
+                    <label>
+                        Cedula:
+                    </label>
+                    <br />
+                    <input
+                        type="text"
+                        className="form-control"
+                        name="userID"
+                        onChange={this.handleChange}
+                    />
+                    <br />
+                    <label>
+                        Password:
+                    </label>
+                    <br />
+                    <input
+                        type="password"
+                        className="form-control"
+                        name="password"
+                        onChange={this.handleChange}
+                    />
+                    <br />
+                    <button className="btn btn-primary" onClick={this.auth}>
+                        Iniciar sesión
+                    </button>
                 </div>
+
             </div>
         );
     }
-}
+};
+
 export default Login;
